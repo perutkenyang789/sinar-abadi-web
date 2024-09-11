0c5852f5d5f46be5a13c3645630cc8c718296e59
Ida Made Revindra Dikta Mahendra <idamaderevindra05@gmail.com>
2024-09-11 08:33:13 +0700

    Connect View dengan Templates

--- main/views.py
+++ main/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def show_main(request):
+    context = {
+        'company' : 'Toko Sinar Abadi',
+        'owner_name': 'Ida Made Revindra Dikta Mahendra',
+        'owner_class': 'kelas PBP C'
+    }
+
+    return render(request, "main.html", context)

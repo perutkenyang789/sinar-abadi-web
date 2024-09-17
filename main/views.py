b30abbf12d3b101ede9ee2f2d1db41aba341b4a9
Ida Made Revindra Dikta Mahendra <idamaderevindra05@gmail.com>
2024-09-17 12:10:59 +0700

    Add functions to view data in xml and json format, also with ID

--- main/views.py
+++ main/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from main.forms import ProductForm
 from main.models import Product
+from django.http import HttpResponse
+from django.core import serializers
 
 def show_main(request):
     store_products = Product.objects.all()
@@ -23,3 +25,19 @@ def create_product(request):
 
     context = {'form': form}
     return render(request, "create_product.html", context)
+
+def show_xml(request):
+    data = Product.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = Product.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_by_id(request, id):
+    data = Product.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json_by_id(request, id):
+    data = Product.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")

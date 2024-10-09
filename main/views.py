19f43fed4271c6ea26ef01e3328403f4d8c8d01a
Ida Made Revindra Dikta Mahendra <idamaderevindra05@gmail.com>
2024-10-09 10:13:50 +0700

    Modify to view product data with fetch() API

--- main/views.py
+++ main/views.py
@@ -12,14 +12,11 @@ from django.urls import reverse
 
 @login_required(login_url='/login')
 def show_main(request):
-    store_products = Product.objects.filter(user=request.user)
-
     context = {
         'name': request.user.username,
         'company' : 'Toko Sinar Abadi',
         'owner_name': 'Ida Made Revindra Dikta Mahendra',
         'owner_class': 'kelas PBP C',
-        'store_products': store_products,
         'last_login': request.COOKIES.get('last_login', 'Have not logged in before'),
     }
 
@@ -38,11 +35,11 @@ def create_product(request):
     return render(request, "create_product.html", context)
 
 def show_xml(request):
-    data = Product.objects.all()
+    data = Product.objects.filter(user=request.user)
     return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
 
 def show_json(request):
-    data = Product.objects.all()
+    data = Product.objects.filter(user=request.user)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
 def show_xml_by_id(request, id):
